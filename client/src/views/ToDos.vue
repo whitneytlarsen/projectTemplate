28c84cbbabf047641354c4f766d9cccbdbf85585
whitneytlarsen <whitneytlarsen@gmail.com>
2019-02-06 13:55:01 -0800

    Added Todos from last lab

--- client/src/views/ToDos.vue
+++ client/src/views/ToDos.vue
@@ -1,39 +1,56 @@
-<template>
-  <div class="todos">
-    <div>Hi from todos</div>
 
-    <div v-for="(todo, index) in mytodos" v-bind:key="index">
-      <span>{{ todo.name }}</span>
-      <span>{{ todo.duedate }}</span>
-    </div>
-    <button class="button" v-on:click="addTodoItem">Add</button>
+<template>
+  <div class='todos'>
+    <div>Hi from Todos</div>
+      <div v-for="(todo, index) in mytodos" v-bind:key="index">
+          <span>{{todo.name}}</span>
+          <span>{{todo.duedate}}</span>
+      </div>
+      <div class="buttons">
+        <a class="button is-primary" v-on:click="showNewToDoModal()">
+          <strong>Add</strong>
+         </a>
+      </div>
+      <router-view/>
+      <NewToDo v-bind:is-showing="showNewToDo" v-on:success="successNewToDo()" v-on:cancel="cancelNewToDo()"/>
   </div>
-  
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from 'vue';
 import { Component } from "vue-property-decorator";
-@Component
+import Modal from "../components/Modal.vue";
+import NewToDo from "../components/NewToDo.vue";
+
+@Component({
+  components: { NewToDo }
+})
+
 export default class ToDos extends Vue {
-  mytodos: ToDo[] = [
-    { name: "Tod one", duedate: undefined },
-    { name: "todo two", duedate: undefined },
-    { name: "todo three", duedate: undefined }
-  ];
-  addTodoItem() {
-    this.mytodos.push({
-      name: `todo${new Date().getTime()}`,
-      duedate: undefined
-    });
-  }
+  public showNewToDo: boolean = false;
+    mytodos: Todo[] = [
+        {name: "Tod one", duedate: undefined },
+        {name: "todo two", duedate: undefined}
+    ];
+    addTodoItem(){
+        this.mytodos.push({
+            name: `todo${new Date().getTime()}`,
+            duedate: undefined
+            });
+    }
+    showNewToDoModal(){
+      this.showNewToDo = true;
+    }
+    successNewToDo(){
+      this.showNewToDo = false;
+    }
+    cancelNewToDo(){
+      this.showNewToDo = false;
+    }
 }
 
-interface ToDo {
-  name: string;
-  duedate: Date | undefined;
+export interface Todo {
+    name: string;
+    duedate: Date | undefined;
 }
 </script>
-
-<style scoped>
-</style>
